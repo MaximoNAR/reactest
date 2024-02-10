48358e61d6370f00478adb8af61a922e522c0cf7
nahuelflores04 <timeboost04@gmail.com>
2024-02-10 14:54:24 -0300

    creacion de login

--- client/src/connection_api.js
+++ client/src/connection_api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
     withCredentials: true,
-    baseURL: process.env.REACT_APP_APT_URL
+    baseURL: process.env.REACT_APP_API_URL
 })
 
 export default instance;
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,32 @@
 import React from "react";
+import instance from "../connection_api";
 
 const Login = () => {
-return (
+  return (
     <>
-    Login 
+      <form>
+        <div>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            placeholder="Ingresar usuario"
+          />
+        </div>
+        <br />
+        <div>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Ingresar contraseña"
+          />
+        </div>
+        <br />
+        <button>Ingresar</button>
+      </form>
     </>
-)
+  );
 };
 
 export default Login;
