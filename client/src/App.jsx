83cbd7b996eedc99b269e41248506f6b5b2d6f5a
Franco <heber01mm@gmail.co>
2024-02-10 01:19:25 -0300

    estructurando el cliente con react

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import {Route, Routes, HashRouter as BrowserRouter } from "react-router-dom";
+
+const  App = () => {
+  return (
+   <>
+   <h1>
+    
+   </h1>
+   
+   </>
+  );
+}
+
+export default App;
