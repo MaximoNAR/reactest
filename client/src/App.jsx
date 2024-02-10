4c4d37f05b41aa5dbea3261b96e7e5713d790388
Franco <heber01mm@gmail.co>
2024-02-10 14:46:33 -0300

    Inicio y Login de app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
 import { Route, Routes, HashRouter as BrowserRouter } from "react-router-dom";
+import Login from "./pages/Login";
+import Inicio from "./pages/inicio";
+
+
 
 const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          {/* <Route path="/login" element={<Login />} />
-          <Route path="/" element={<Inicio />} /> */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Inicio/>} />
         </Routes>
       </BrowserRouter>
     </div>
